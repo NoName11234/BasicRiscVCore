ebde12888e9a90021a517df59da616812da03b4e
NoName112345 <ykonto91@gmail.com>
2024-09-12 19:49:44 +0200

    added parameter list to decoder module

--- src/decoder.sv
+++ src/decoder.sv
@@ -1,5 +1,27 @@
-module decoder(
-    input [31:0] instruction
+module decoder import defines::*(
+    input [31:0] instruction,
+
+    //alu outputs
+    output reg [31:0] alu_immediate,
+    output alu_operand_t operand_a,
+    output alu_operand_t operand_b,
+    output alu_operation_t operation,
+
+    //program counter outputs
+    output pc_load_enable,
+    output pc_count_enable,
+    output pc_preset_value,
+
+    //mainbus
+    output mainbus_alu_select,
+    output mainbus_register_bank_select,
+    output mainbus_memory_select,
+
+    //register bank
+    output bank_load_enable,
+    output reg [4:0] bank_select_in,
+    output reg [4:0] bank_select_out_a,
+    output reg [4:0] bank_select_out_b
 );
 
 endmodule
