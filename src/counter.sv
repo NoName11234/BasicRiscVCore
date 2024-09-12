3dbc2c085f246b6c7e0546bc1d236f97f509ff29
NoName112345 <ykonto91@gmail.com>
2024-09-12 19:48:26 +0200

    renamed input variables

--- src/counter.sv
+++ src/counter.sv
@@ -8,19 +8,19 @@ module counter # (
   input load_en,
   input count_en,
 
-  input [SIZE-1:0] d,
+  input [SIZE-1:0] preset_value,
 
-  output reg [SIZE-1:0] q
+  output reg [SIZE-1:0] counter_value
 );
   
   always @(posedge clk, posedge rst) begin
     if (rst) begin
-      q <= 0;
+      counter_value <= 0;
     end else begin
       if (load_en)
-        q <= d;
+        counter_value <= preset_value;
       else if (count_en)
-        q <= q + 1;
+        counter_value <= counter_value + 1;
     end
   end
 
